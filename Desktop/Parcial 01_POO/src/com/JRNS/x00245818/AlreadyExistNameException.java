6df1728ed2d6a2792d5130ae8c51ab8f863d361a
Jnasser513 <54814817+Jnasser513@users.noreply.github.com>
2020-04-19 23:13:33 -0600

    Añadiendo Excepciones

---
+++ Desktop/Parcial 01_POO/src/com/JRNS/x00245818/AlreadyExistNameException.java
@@ -0,0 +1,7 @@
+package com.JRNS.x00245818;
+
+public class AlreadyExistNameException extends Exception {
+    public AlreadyExistNameException(String message) {
+        super(message);
+    }
+}
---
+++ Desktop/Parcial 01_POO/src/com/JRNS/x00245818/NotExistingEmployeeException.java
@@ -0,0 +1,7 @@
+package com.JRNS.x00245818;
+
+public class NotExistingEmployeeException extends Exception {
+    public NotExistingEmployeeException(String message){
+        super(message);
+    }
+}
